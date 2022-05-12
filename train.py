036be51a6fd1d2ae659f4c9a9c0e01d5a732c317
hhurchand <h.hurchand@gmail.com>
2022-05-12 13:51:24 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import mlflow
-remote_server_uri = "http://localhost:1234/" # set to your server URI
-mlflow.set_tracking_uri(remote_server_uri)
+#remote_server_uri = "http://localhost:1234/" # set to your server URI
+#mlflow.set_tracking_uri(remote_server_uri)
 # Note: on Databricks, the experiment name passed to mlflow_set_experiment must be a
 # valid path in the workspace
-mlflow.set_experiment("/my-experiment")
-with mlflow.start_run():
-    mlflow.log_param("a", 1)
-    mlflow.log_metric("b", 2)
+#mlflow.set_experiment("/my-experiment")
+#with mlflow.start_run():
+#    mlflow.log_param("a", 1)
+#    mlflow.log_metric("b", 2)
+print("Hello)
